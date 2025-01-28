01fdc985ad611505d960c01b14748b843c63cf80
Daniel Xavier <dxavier194@gmail.com>
2025-01-28 20:21:59 -0300

    Iniciando projeto

---
+++ src/main/kotlin/com/danielxavier/invoiceLogic/InvoiceLogicApplication.kt
@@ -0,0 +1,11 @@
+package com.danielxavier.invoiceLogic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InvoiceLogicApplication
+
+fun main(args: Array<String>) {
+	runApplication<InvoiceLogicApplication>(*args)
+}
---
+++ src/test/kotlin/com/danielxavier/invoiceLogic/InvoiceLogicApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.danielxavier.invoiceLogic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InvoiceLogicApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
